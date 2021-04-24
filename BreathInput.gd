ba66ee95660af491ae1bd1a077b907beba103189
Eilandis <cyrildefaria@gmail.com>
2021-04-25 01:00:49 +0200

    Inputs UI

---
+++ BreathInput.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var MARGIN_X = 10
+var INITIAL_POSITION = Vector2(113, 7)
+var INPUTS_GROUP = "BreathInputs"
+
+var input_rotation = {
+    'ui_up': 0,
+    'ui_right': 90,
+    'ui_down': 180,
+    'ui_left': 270,
+}
+
+var arrows = {
+    -1: load("res://assets/arrow_red.png"),
+    0: load("res://assets/arrow.png"),
+    1: load("res://assets/arrow_white.png")
+}
+
+func _display_keys_to_press(keys_pressed):
+    get_tree().call_group(INPUTS_GROUP, "queue_free")
+    for i in len(keys_pressed):
+        var input = keys_pressed[i][0]
+        var result = keys_pressed[i][1]
+        var sprite = Sprite.new()
+        sprite.set_texture(arrows[result])
+        sprite.position = INITIAL_POSITION
+        sprite.position = Vector2(i * (sprite.texture.get_size().x * sprite.scale.x + MARGIN_X), 0)
+        sprite.rotation_degrees = input_rotation[input]
+        sprite.add_to_group(INPUTS_GROUP)
+        add_child(sprite)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
