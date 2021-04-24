8bdf8601df4954cd5d8b4773107040cbecd0ffe1
Eilandis <cyrildefaria@gmail.com>
2021-04-24 21:36:39 +0200

    Breathing Signals

---
+++ Main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    $Rythm.connect("keys_pressed_signal", $GUI, "_display_keys_to_press")
+    $Rythm.connect("oxygen_signal", 
+                   $GUI/HBoxContainer/ItemsOxygen/Oxygen/Oxygen, 
+                   "_update_oxygen")
+    
+    
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
