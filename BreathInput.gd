adc444197ee60c5a108ecfa4b4b0c264b2d7bb0f
Eilandis <cyrildefaria@gmail.com>
2021-04-25 12:21:01 +0200

    Refactored the rythm code.

--- BreathInput.gd
+++ BreathInput.gd
@@ -18,6 +18,18 @@ var arrows = {
 }
 
 func _display_keys_to_press(keys_pressed):
+    """Voilà à quoi ressemblera keys_to_display :
+        [
+            ["ui_left", 1], 
+            ["ui_right", -1], 
+            ["ui_left", 0],
+        ]    
+        Cela signifie :
+        - que le rythme pour respirer est gauche - droite - gauche
+        - que la première touche (gauche) a été pressée dans le bon timing (1)
+        - que la deuxième touche (droite) n'a pas été pressée (-1)
+        - que la troisième touche (gauche) n'est pas encore arrivée (0)
+    """
     get_tree().call_group(INPUTS_GROUP, "queue_free")
     for i in len(keys_pressed):
         var input = keys_pressed[i][0]
--- Rythm.gd
+++ Rythm.gd
@@ -9,17 +9,22 @@ export var MAX_OXYGEN = 5
 
 var oxygen = MAX_OXYGEN
 
-# Pour chaque biome : X touches à saisir séquentiellement
+# Pour chaque biome : X touches à saisir séquentiellement pour respirer
 var biome_inputs = [
-    ['ui_left', 'ui_right']
+    ['ui_left', 'ui_right', 'ui_up']
 ]
 var biome_index = 0
-var current_inputs = biome_inputs[biome_index]
 var input_index = 0
-var current_input = current_inputs[input_index]
 var keys_pressed = []
-var input_changed = false
 var input_was_pressed = false
+var rythm_fucked = false
+
+func get_current_inputs():
+    return biome_inputs[biome_index]
+    
+func get_current_input():
+    var current_inputs = self.get_current_inputs()
+    return current_inputs[input_index]
 
 func _reset_keys_pressed():
     """"
@@ -29,49 +34,45 @@ func _reset_keys_pressed():
     - not pressed (or in the wrong timing) : -1
     """
     keys_pressed = []
-    for input in current_inputs:
+    for input in self.get_current_inputs():
         keys_pressed.append([input, 0])
     input_index = 0
+    $Key.text = self.get_current_input()
+    rythm_fucked = false
+
+func update_keys_pressed(result):
+    for i in len(keys_pressed):
+        keys_pressed[i][1] = 0
+    keys_pressed[input_index][1] = result
 
 func _ready():
     self._reset_keys_pressed()
-    print("Ready : " + str(keys_pressed))
     emit_signal("keys_pressed_signal", keys_pressed)
     $KeyTimer.connect("timeout", self, "_on_key_timer_timeout")
-    $KeyTimer.wait_time = RYTHM_DURATION / len(current_inputs)
+    $KeyTimer.wait_time = RYTHM_DURATION / len(self.get_current_inputs())
     $KeyTimer.start()
 #    $AcceptanceDelay.wait_time = ACCEPTANCE_DELAY
 #    $AcceptanceDelay.one_shot = true
 #    $AcceptanceDelay.connect("timeout", self, "_on_timeout_acceptance_delay")
 
 func rythm_result():
-    var rythm_fucked = false
-    for key_pressed in keys_pressed:
-        if key_pressed[1] == -1 :
-            rythm_fucked = true
-        break
     emit_signal("oxygen_signal", rythm_fucked)
-    if rythm_fucked:
-        print("Oxygen missing : " + str(keys_pressed))
-    else:
-        print("Full combo ! " + str(keys_pressed))
     self._reset_keys_pressed()
-    emit_signal("keys_pressed_signal", keys_pressed)
-    print("Reset : " + str(keys_pressed))
 
 func _on_key_timer_timeout():
+    var current_inputs = self.get_current_inputs()
     if not input_was_pressed:
-        keys_pressed[input_index][1] = -1
+        self.update_keys_pressed(-1)
+        rythm_fucked = true
+    emit_signal("keys_pressed_signal", keys_pressed)
+    print("Oops raté : " + str(keys_pressed))
+    
+    # Si toutes les notes du rythme ont été jouées
     if input_index == current_inputs.size() - 1:
-        # Toutes les notes du rythme ont été jouées
         self.rythm_result()
     else:
-        emit_signal("keys_pressed_signal", keys_pressed)
-        print("Oops raté : " + str(keys_pressed))
         input_index += 1
-    current_input = current_inputs[input_index]
-    $Key.text = current_input
-    input_changed = true
+    $Key.text = self.get_current_input()
     input_was_pressed = false
 #    $AcceptanceDelay.start()
     
@@ -81,16 +82,16 @@ func _on_key_timer_timeout():
 #        keys_pressed[input_index] = -1
 
 func _process(delta):
-    if input_changed and not input_was_pressed:
-        if Input.is_action_pressed(current_input):
+    if not input_was_pressed:
+        if Input.is_action_pressed(self.get_current_input()):
             input_was_pressed = true
-            keys_pressed[input_index][1] = 1
+            self.update_keys_pressed(1)
             emit_signal("keys_pressed_signal", keys_pressed)
             print("Nice : " + str(keys_pressed))
 
 # TODO : Lorsqu'on change de biome, mettre à jour :
 # - le KeyTimer.wait_time
 # - biome_index
-# - current_inputs
+# - input_index
 # - keys_pressed
 # - signaler à l'UI que les inputs ont changé
