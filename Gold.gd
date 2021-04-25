eee773cb7ef6b96b997002fc72eb0ac83a7d1bf2
Erilhir <jacob.marie.andrea@gmail.com>
2021-04-25 15:52:55 +0200

    Des SOUS !
    Ajout du son quand on récolte des pièces

--- Gold.gd
+++ Gold.gd
@@ -3,5 +3,13 @@ extends Area2D
 signal coin_collected
 
 func _on_body_entered(body):
+    """
+        Le sprite.visible = false permet de ne plus afficher la pièce à 
+        l'écran le temps que la musique se joue. Sinon, la pièce reste 
+        affichée jusqu'à la fin de la musique, ce qui créé un décalage
+    """
+    $Sprite.visible = false
+    $Sound.play()
     emit_signal("coin_collected", 1)
+    yield($Sound, "finished")
     queue_free()
