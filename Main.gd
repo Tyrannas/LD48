15e34a580a8f85807a1939e1762913e5105bb51a
Eilandis <cyrildefaria@gmail.com>
2021-04-26 12:28:26 +0200

    Fade in / Fade out during biome transitions. Changed the AudioPlayer2D to AudioPlayer

--- Main.gd
+++ Main.gd
@@ -9,33 +9,43 @@ var RANDOM_X_OFFSET = 23
 var background_size
 var biome_index = 0
 
+var fade_out_duration = 3.0
+var fade_in_duration = 1.5
+var fade_type = 1 # TRANS_SINE
+onready var current_music_player = get_node("Player/Music")
+
 var BiomeTransition = preload("res://BiomeTransition.tscn")
 
-var FIRST_BIOME_DEPTH = 50
-var SECOND_BIOME_DEPTH = 100
 #Pour chaque biome : 
 #- clef : profondeur à laquelle commence le biome
 #- valeur : touches à saisir séquentiellement pour respirer
-var biome_inputs = [
-    {0: ['ui_down', 'ui_down', 'ui_down']},
-    {FIRST_BIOME_DEPTH: ['ui_left', 'ui_left', 'ui_left', 'ui_left']},
-    {SECOND_BIOME_DEPTH: ['ui_up', 'ui_up', 'ui_up']}
+var biome_infos = [
+    {
+        'depth': 0,
+        'inputs': ['ui_down', 'ui_down', 'ui_down'],
+        'music': "music.ogg"
+    },
+    {
+        'depth': 50,
+        'inputs': ['ui_left', 'ui_left', 'ui_left', 'ui_left'],
+        'music': "pom_pom_pom.wav"
+    },
+    {
+        'depth': 100,
+        'inputs': ['ui_up', 'ui_up', 'ui_up'],
+        'music': "music.ogg"
+    },
 ]
-#var biome_musics = [
-#    {0: },
-#    {FIRST_BIOME_DEPTH: ['ui_left', 'ui_left', 'ui_left', 'ui_left']},
-#    {SECOND_BIOME_DEPTH: ['ui_up', 'ui_up', 'ui_up']}
-#]
 
 func get_biome_inputs(index):
-    return biome_inputs[index].values()[0]
+    return biome_infos[index]['inputs']
 
 func get_biome_depth(index):
-    return biome_inputs[index].keys()[0]
-
+    return biome_infos[index]['depth']
+    
 func instantiate_biome_delimiters():
     # we skip the first biome
-    for biome_i in len(biome_inputs):
+    for biome_i in len(biome_infos):
         if biome_i == 0:
             continue
         var biome_depth = self.get_biome_depth(biome_i) * 10
@@ -64,6 +74,7 @@ func _ready():
                    $Player/Camera2D/CanvasLayer/GUI/VBoxContainer/HBoxContainer/ItemsOxygen/Oxygen/Oxygen, 
                    "_update_oxygen")
     self.connect("biome_change", $Rythm, "_update_biome_inputs")
+    $Player/FadeOut.connect('tween_completed', self, '_stop_music')  
     
     # TODO : A retirer si on instancie les pièces de manière auto
     get_tree().call_group("Gold", "connect", "coin_collected", 
@@ -82,15 +93,41 @@ func _ready():
     if Global.is_retry == true : 
         $StartTimer.start(1)
 
+func fade_out(stream_player):
+    var tween_out = $Player/FadeOut
+    # tween music volume down to 0
+    tween_out.interpolate_property(stream_player, "volume_db", 0, -80, fade_out_duration, fade_type, Tween.EASE_IN, 0)
+    tween_out.start()
+
+func fade_in():
+    var audio_file = "res://assets/music/" + biome_infos[biome_index]['music']
+    if File.new().file_exists(audio_file):
+        var new_music_player = AudioStreamPlayer.new()
+        var sfx = load(audio_file)
+        new_music_player.set_stream(sfx)
+        new_music_player.volume_db = -80
+        add_child_below_node($Player, new_music_player)
+        new_music_player.play()
+        var tween_in = $Player/FadeIn
+         # tween music volume back to 0
+        tween_in.interpolate_property(new_music_player, "volume_db", -80, 0, fade_in_duration, fade_type, Tween.EASE_OUT, 0)
+        tween_in.start()
+        current_music_player = new_music_player
+       
+func _stop_music(object, _key):
+    # stop the music -- otherwise it continues to run at silent volume
+    object.stop()
+
 func _update_biome(_body):
     biome_index += 1
+    self.fade_out(current_music_player)
+    self.fade_in()
     emit_signal("biome_change", self.get_biome_inputs(biome_index))
 
 func _process(delta):    
     if $ReadyText.visible:
         # Affichage du temps restant avant de démarrer le jeu
         $ReadyText.text = "Ready ? " + str(int($StartTimer.time_left))
-
 
 func _on_StartTimer_timeout():
     new_game()
