ee559003695de2f2e5612518dba21a9fb3dacd63
Erilhir <jacob.marie.andrea@gmail.com>
2021-04-26 22:41:48 +0200

    Easter Egg
    FINAL PUSH !!!

--- GameOver.gd
+++ GameOver.gd
@@ -1,14 +1,29 @@
 extends CanvasLayer
 
 func _ready():
+    
+    $Score.text = "Score : " + str(Global.score)
+    $Depth.text = "Depth : " + str(Global.depth) + "m"
+    
     if Global.is_dead == true:
         $GameOver.visible = true
         $Text.text = "Game over"
     else :
         $GameEnd.visible = true
         $Text.text = "Hurray !"
-    $Score.text = "Score : " + str(Global.score)
-    $Depth.text = "Depth : " + str(Global.depth) + "m"
+        var t = Timer.new()
+        t.set_wait_time(1.5)
+        t.set_one_shot(true)
+        self.add_child(t)
+        t.start()
+        yield(t, "timeout")
+        emit_signal("end_game")
+        $GameEnd/AnimationPlayer.play("Remove_the_head")
+        $GameEnd/EasterEgg.visible = true
+        
+    
+    
+    
 
 
 func _on_Retry_pressed():
