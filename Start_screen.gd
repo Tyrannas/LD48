d045921e157ea37190aa596ca45031411477307c
Erilhir <jacob.marie.andrea@gmail.com>
2021-04-26 17:38:54 +0200

    Page principale

--- Start_screen.gd
+++ Start_screen.gd
@@ -4,6 +4,9 @@ signal start_game
 
 func _ready():
     $Start.connect("pressed", self, "_on_Button_pressed")
+    $ArrowLeft.play()
+    $ArrowRight.play()
     
 func _on_Button_pressed():
     get_tree().change_scene("res://Main.tscn")
+
