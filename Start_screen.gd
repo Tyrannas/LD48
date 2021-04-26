82abe544860ca6704a83109c4510427eda702762
Erilhir <jacob.marie.andrea@gmail.com>
2021-04-26 21:59:19 +0200

    New scene pour débilos

--- Start_screen.gd
+++ Start_screen.gd
@@ -10,6 +10,15 @@ func _ready():
     $Keys_left.play()
     $Keys_right.play()
     
+    $Left_Q.play()
+    $Left_A.play()
+    $Right_D.play()
+    
+    $AnimationPlayer.play("Player")
+    
+    
 func _on_Button_pressed():
     get_tree().change_scene("res://Main.tscn")
+
+
 
