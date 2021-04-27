0f8dbcf2cd007df84437aad5712cb50522945b19
Tyrannas <someonefefe@gmail.com>
2021-04-27 23:04:04 +0200

    rythm window

--- Rythm.gd
+++ Rythm.gd
@@ -11,6 +11,7 @@ var keys_pressed = []
 var input_was_pressed = false
 var rythm_fucked = false
 var new_biome_infos = {}
+var preserve_first = false
 
 func _ready():
     $KeyTimer.connect("timeout", self, "_on_key_timer_timeout")    
@@ -19,16 +20,21 @@ func get_current_input():
     if biome_inputs:
         return biome_inputs[input_index]
 
+func _get_next_index():
+    return input_index + 1 if input_index < len(biome_inputs) - 1 else 0
+    
 func _reset_keys_pressed():
     keys_pressed = []
     for i in len(biome_inputs):
+        var result = 1 if i == 0 and preserve_first else 0
         keys_pressed.append({
             "key": biome_inputs[i],
-            "result": 0,
+            "result": result,
             "is_current": i == 0,
         })
     input_index = 0
     rythm_fucked = false
+    preserve_first = false
     emit_signal("keys_pressed_signal", keys_pressed)
 
 func update_keys_pressed_result(result, index=input_index):
@@ -55,41 +61,62 @@ func _update_biome_inputs(new_inputs, bpm):
     new_biome_infos = {'inputs': new_inputs, 'bpm': bpm}
     if not biome_inputs:
         # Initialisation for the first biome
-        self._update_biome_infos()
+        _update_biome_infos()
         $KeyTimer.start()
-        self._reset_keys_pressed()
+        _reset_keys_pressed()
 
 func is_last_note_of_rythm():
     return input_index == biome_inputs.size() - 1
 
 func _on_key_timer_timeout():
-    self.update_keys_pressed_current(input_index+1)
+    update_keys_pressed_current(input_index+1)
     if not input_was_pressed:
-        self.update_keys_pressed_result(-1)
+        update_keys_pressed_result(-1)
     # Si toutes les notes du rythme ont été jouées
-    if self.is_last_note_of_rythm():
+    if is_last_note_of_rythm():
         emit_signal("oxygen_signal", rythm_fucked)
         if new_biome_infos:
-            self._update_biome_infos()
-        self._reset_keys_pressed()
+            _update_biome_infos()
+        _reset_keys_pressed()
     else:
         emit_signal("keys_pressed_signal", keys_pressed)
         input_index += 1
-    input_was_pressed = false
-
+    if keys_pressed[input_index]["result"] != 1:
+        input_was_pressed = false
+        
+func __get_time():
+    var time = OS.get_time()
+    return String(time.hour) +":"+String(time.minute)+":"+String(time.second)
+    
 func _process(_delta):
+    # TODO: handle feedback when rythmfucked is true
     if not rythm_fucked:
         for input in ["ui_left", "ui_right", "ui_up", "ui_down"]:
             if Input.is_action_just_pressed(input):
                 if not input_was_pressed:
-                    if input == self.get_current_input():
-                        self.update_keys_pressed_result(1)
-                    else:
-                        self.update_keys_pressed_result(-1)
+                    # si une touche n'a pas déjà été validée pour cet interval
+                    _validate_input(input)
                     input_was_pressed = true
                 else:
                     # If the player taps too early, the next note is wrong
-                    # (except if it's the last note of the combo)
-                    if not self.is_last_note_of_rythm():
-                        self.update_keys_pressed_result(-1, input_index + 1)
+                    # except if time left to complete <= wait_time / 3 and it's the right key
+                    # then accept the key
+                    if $KeyTimer.time_left < $KeyTimer.wait_time / 3:
+                        _validate_input(input, _get_next_index())
+                    else:
+                        # else it's too early to validate the next key so set it failed
+                        update_keys_pressed_result(-1, _get_next_index())
+
                 emit_signal("keys_pressed_signal", keys_pressed)
+
+func _validate_input(input, index=input_index):
+    var input_to_check = get_current_input() if index == input_index  else biome_inputs[index]
+    # est ce que c'est la bonne touche?
+    if input == input_to_check:
+        update_keys_pressed_result(1, index)
+        if index != input_index and index == 0:
+            # if the next key is the first one of the next sequence and was early pressed
+            # prevent it from being reseted
+            preserve_first = true
+    else:
+        update_keys_pressed_result(-1, index)
